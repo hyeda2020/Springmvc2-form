b2d23379fbc6a5c970d7513521023e9e68f338e5
hyeda2020 <hyeda2020@gmail.com>
2024-02-16 00:17:19 +0900

    타임리프 - 스프링 통합과 폼(아이템 폼에 라디오 버튼, 셀렉트 박스 추가)

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,7 +1,9 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +27,9 @@ public class FormItemController {
     /**
      * <@ModelAttribute의 특별한 사용법>
      * 이렇게 @ModelAttribute를 컨트롤러 내의 별도 메서드에 적용하면
-     * 해당 컨트롤러를 요청할 때 regions에서 반환한 값이 자동으로 모델(model)에 담기게 됨!
+     * 해당 컨트롤러를 요청할 때 regions에서 반환한 값이 자동으로 모델(model)에 담기게 됨
+     * 단, 컨트롤러 내 메서드가 호출될 때마다 해당 객체가 계속 생성되므로
+     * 다른 곳에서 미리 한번만 생성해놓고 재사용하는 것이 바람직함
      */
     @ModelAttribute("regions")
     public Map<String, String> regions() {
@@ -33,6 +38,20 @@ public class FormItemController {
         regions.put("BUSAN", "부산");
         regions.put("JEJU", "제주");
         return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        return ItemType.values(); // Enum 클래스의 .values() 메서드는 해당 Enum 클래스의 모든 정보를 배열로 반환
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린배송"));
+        return deliveryCodes;
     }
 
     @GetMapping
@@ -59,6 +78,7 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}", item.getOpen());
         log.info("item.regions={}", item.getRegions());
+        log.info("item.itemTypes={}", item.getItemType());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
